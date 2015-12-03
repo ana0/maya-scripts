0be7a213b7a8b554b9d5a7cbe42e6c57f5405124
ana0 <sarah.anne.friend@gmail.com>
2015-12-03 14:54:41 -0500

    place cameras with rotation

--- material_swapper.py
+++ material_swapper.py
@@ -15,7 +15,7 @@ import math
 
 def generate_camera_points(angle, radius):
 	angles = [math.radians(i) for i in range(360) if i % angle == 0]
-	points = [(radius*(math.cos(i)), radius*(math.sin(i))) for i in angles]
+	points = [(radius*(math.cos(i)), radius*(math.sin(i)), math.degrees(i)) for i in angles]
 	return points
 
 def place_cameras(points):
@@ -23,7 +23,9 @@ def place_cameras(points):
 		cmds.camera(centerOfInterest=5, focalLength=35, lensSqueezeRatio=1, cameraScale=1, horizontalFilmAperture=1.41732, horizontalFilmOffset=0, 
 			verticalFilmAperture=0.94488, verticalFilmOffset=0, filmFit="fill", overscan=1, motionBlur=0, shutterAngle=144, nearClipPlane=0.1,
 			farClipPlane=10000, orthographic=0, orthographicWidth=30, panZoomEnabled=0, horizontalPan=0, verticalPan=0, zoom=1)
-		cmds.move(i[0], 5, i[1])
+		cmds.move(i[0], 5.0, i[1])
+		#cmds.rotate(0.0, i[2]+90, 0.0, absolute=True, objectSpace=True, forceOrderXYZ=True)
+		cmds.viewPlace(lookAt=[0, 0, 0])
 
 
 
@@ -31,3 +33,5 @@ def place_cameras(points):
 
 cats = generate_camera_points(6, 17)
 place_cameras(cats)
+# for i in cats:
+# 	print i
